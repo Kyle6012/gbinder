e48f27b0e710e41df9f9d586f2af0e658e87fb7f
Meshack Bahati <102649537+Kyle6012@users.noreply.github.com>
2025-04-29 13:25:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-import sys, subprocess
-from distutils.core import setup, Extension
-
+import sys
+import subprocess
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
 def pkgconfig(package, kw):
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    output = subprocess.getoutput(
-        'pkg-config --cflags --libs {}'.format(package))
+    output = subprocess.getoutput(f'pkg-config --cflags --libs {package}')
     for token in output.strip().split():
-        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+        if token[:2] in flag_map:
+            kw.setdefault(flag_map[token[:2]], []).append(token[2:])
     return kw
 
 USE_CYTHON = False
@@ -15,28 +16,33 @@ if "--cython" in sys.argv:
     sys.argv.remove("--cython")
     USE_CYTHON = True
 
-file_ext = ".pyx" if USE_CYTHON else ".c"
+ext = ".pyx" if USE_CYTHON else ".c"
+ext_kwargs = {'sources': [f"gbinder{ext}"]}
+ext_kwargs = pkgconfig("libgbinder", ext_kwargs)
 
-extension_kwargs = { 'sources': ["gbinder" + file_ext] }
-extension_kwargs = pkgconfig('libgbinder', extension_kwargs)
-if None in extension_kwargs:
-    del extension_kwargs[None]
-extensions = [Extension('gbinder', **extension_kwargs)]
+extensions = [Extension("gbinder", **ext_kwargs)]
 
 if USE_CYTHON:
     from Cython.Build import cythonize
-    extensions = cythonize(extensions, compiler_directives={
-                           'language_level': "3"})
+    extensions = cythonize(extensions, compiler_directives={"language_level": "3"})
 
 setup(
     name="gbinder",
-    description="""Cython extension module for C++ gbinder functions""",
-    version="1.1.2",
-    author="Erfan Abdi",  # Your name
-    author_email="erfangplus@gmail.com",  # Your email
+    version="1.2.1",
+    description="Cython extension module for C++ gbinder functions",
+    author="Erfan Abdi",
+    author_email="erfangplus@gmail.com",
+    maintainer="Meshack Bahati",
+    maintainer_email="bahatikylemeshack@gmail.com",
+    license="GPLv3",
     url="https://github.com/Kyle6012/gbinder",
-    license="GPL3",
     ext_modules=extensions,
-    maintainer="Meshack Bahati",  
-    maintainer_email="bahatikylemeshack@gmail.com"
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Cython",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["Cython", "C++", "gbinder", "extension module"],
+    python_requires=">=3.6",
 )
